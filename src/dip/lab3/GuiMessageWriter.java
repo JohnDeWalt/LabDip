9abc8caf252b638dadb2e583c8024dbf2e48f48a
JohnDeWalt <Jackie@10.30.12.25>
2017-10-03 01:23:45 -0500

    Adding in a Gui message

---
+++ src/dip/lab3/GuiMessageWriter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Jackie
+ */
+public class GuiMessageWriter implements MessageOutput {
+    
+    public void writeln( String line ) {
+            JOptionPane.showMessageDialog(null,"I hope this is working");
+    }
+}
